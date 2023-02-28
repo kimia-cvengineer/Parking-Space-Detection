c9e70d337dc3264ec51c9093a74b85fba897e924
kimia <kajalafshari@yahoo.com>
2023-02-27 23:29:34 -0800

    updated print freq in evaluation

--- utils_funcs/engine.py
+++ utils_funcs/engine.py
@@ -88,7 +88,7 @@ def evaluate(model, data_loader, res, device):
     coco = get_coco_api_from_dataset(data_loader.dataset)
     iou_types = _get_iou_types(model)
     coco_evaluator = CocoEvaluator(coco, iou_types)
-    for images, targets in metric_logger.log_every(data_loader, 100, header):
+    for images, targets in metric_logger.log_every(data_loader, 10, header):
         # preprocess image
         res_images = transforms.preprocess(images, device=device, res=res)
 
