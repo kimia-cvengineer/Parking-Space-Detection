49b1a0a5371199a9b72aa59467fb436f55437124
kimia <kajalafshari@yahoo.com>
2023-03-01 21:14:56 -0800

    changed MultiRandomRotation bug fixed

--- utils_funcs/transforms.py
+++ utils_funcs/transforms.py
@@ -222,7 +222,7 @@ class MultiRandomRotation(T.RandomRotation):
         # create rotation matrix
         angle_rad = torch.tensor((angle / 180.0) * 3.141592)
         R = torch.tensor([[torch.cos(angle_rad), -torch.sin(angle_rad)],
-                          [torch.sin(angle_rad), torch.cos(angle_rad)]], dtype=torch.float, device=device)
+                          [torch.sin(angle_rad), torch.cos(angle_rad)]], dtype=torch.float)
 
         # move points to an absolute cooridnate system with [0, 0] as the center of the image
         # points = target['boxes'].clone()
