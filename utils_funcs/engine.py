f76e0182f3755a7ab950a982dfffc0e5754b2768
kimia <kajalafshari@yahoo.com>
2023-03-04 11:23:04 -0800

    reduced lr to 1e-5

--- utils_funcs/engine.py
+++ utils_funcs/engine.py
@@ -144,7 +144,7 @@ def evaluate(model, data_loader, resolution, log_dir, device):
     return coco_evaluator
 
 
-def train_model(model, train_ds, valid_ds, test_ds, model_dir, device, lr=8e-5, epochs=30, lr_decay=50, res=None,
+def train_model(model, train_ds, valid_ds, test_ds, model_dir, device, lr=1e-5, epochs=30, lr_decay=50, res=None,
                 verbose=False):
     """
     Trains any model which takes (image, rois) and outputs class_logits.
