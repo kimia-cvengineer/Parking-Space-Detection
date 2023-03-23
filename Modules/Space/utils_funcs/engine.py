0fa32ff02e097ac0d18355de5e670098610b82e0
kimia <kajalafshari@yahoo.com>
2023-03-22 21:57:27 -0700

    A bug fixed

--- Modules/Space/utils_funcs/engine.py
+++ Modules/Space/utils_funcs/engine.py
@@ -110,7 +110,6 @@ def evaluate(model, data_loader, resolution, log_dir, device):
     coco = get_coco_api_from_dataset(data_loader.dataset)
     iou_types = _get_iou_types(model)
     coco_evaluator = CocoEvaluator(coco, iou_types)
-    print("test data loader : ", data_loader)
     i = 0
     for images, targets in metric_logger.log_every(data_loader, 10, log_dir, header):
         # preprocess images
